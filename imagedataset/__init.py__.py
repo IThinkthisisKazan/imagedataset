96f4fa9454dc4f1588f4022b8cc0eccfdf543991
Red Fox <ayartokoev3@gmail.com>
2020-12-06 15:55:11 +0300

    Update __init.py__.py

--- imagedataset/__init.py__.py
+++ imagedataset/__init.py__.py
@@ -1,2 +1,5 @@
-from imagedataset.core import find_and_separate
+import imagedataset 
+import requests
+print(dir(requests))
+#ind_and_separate("часы телефон книги", 100, "C:\\Users\\Red_Fox\\Pictures", 70, 30)
 
